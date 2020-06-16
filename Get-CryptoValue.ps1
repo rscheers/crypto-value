6cb636998b09c415f1be37c673941113703cf398
Remko Scheers <remko@vschijndel.nl>
2020-06-16 20:19:05 +0200

    Changed API and processing of JSON

--- Get-CryptoValue.ps1
+++ Get-CryptoValue.ps1
@@ -1,11 +1,10 @@
-$ApiUrl = 'https://api.coinmarketcap.com/v1/ticker/'
-$Wallet= @'
-Name;Amount
-bitcoin;2.3
-monero;5.14
-ripple;152
-'@ | ConvertFrom-Csv -Delimiter ';'
-$RefCurency = 'USD' #USD,EUR
+$APIKey = Get-Content .\APIKEY.TXT
+#$APIURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=$($APIKey)"
+$APIURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=$($APIKey)"
+
+$Wallet = Import-Csv "wallet.csv" -Delimiter ";"
+
+$RefCurrency = 'EUR' #USD,EUR
 
 $PreContent = @'
 <style>
@@ -27,15 +26,20 @@ $PostContent = @"
 Last Refresh: $(Get-Date)
 "@
 
-$CurrencyFormat = New-Object System.Globalization.CultureInfo("en-US")
+$CurrencyFormat = New-Object System.Globalization.CultureInfo("nl-NL")
+
+#$CurrentValueInUSD = Invoke-WebRequest -Uri "$($ApiUrl)&convert=$RefCurrency" | ConvertFrom-Json
 
 $Wallet| ForEach-Object -Process {
-    $CurrentValueInUSD = ((Invoke-WebRequest -Uri "$($ApiUrl)$($_.Name)/?convert=$RefCurency" | Select-Object -ExpandProperty Content | ConvertFrom-Json) | Select-Object -ExpandProperty price_usd) -as [double]
+    $CryptoCoinSymbol = $_.Symbol
+    $CurrentValueInUSD = Invoke-WebRequest -Uri "$($ApiUrl)&convert=$RefCurrency&symbol=$Cryptocoinsymbol" | ConvertFrom-Json
+    #$CurrentCryptoValue = ($CurrentValueInUSD.Data | Where-Object{$_.Symbol -eq ($CryptoCoin.symbol)}).quote.USD.price -as [double]
+    $CurrentCryptoValue = $CurrentValueInUSD.Data.$CryptoCoinSymbol.quote.$RefCurrency.price -as [double]
     New-Object -TypeName PSObject -Property @{
         Currency = $_.Name
-        ValueInUSD = $CurrentValueInUSD.ToString('c',$CurrencyFormat)
-        ValueInWallet = ($CurrentValueInUSD * $_.Amount).ToString('c',$CurrencyFormat)
+        ValueInUSD = $CurrentCryptoValue.ToString('c',$CurrencyFormat)
+        ValueInWallet = ($CurrentCryptoValue * $_.Amount).ToString('c',$CurrencyFormat)
         Amount = $_.Amount
     }
-    $CurrentValueInUSD = $null
+    $CurrentCryptoValue = $null
 } | ConvertTo-Html -PreContent $PreContent -PostContent $PostContent -Title 'Crypto Coin Dashboard' | Out-File -FilePath wallet.htm
